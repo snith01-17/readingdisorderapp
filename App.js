0fd228a52b39f059f3006416bf8737622cdf0358
Suriya N <75084434+snith01-17@users.noreply.github.com>
2021-11-04 16:23:23 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSoundButton';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+      phonicSounds: [],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#000000'}
+          centerComponent={{
+            text: 'ℝ𝕖𝕒𝕕𝕚𝕟𝕘 𝔻𝕚𝕤𝕠𝕣𝕕𝕖𝕣 𝔸𝕡𝕡',
+            style: { color: '#fff', fontSize: 17 },
+          }}
+        />
+
+       <Image
+          source={require("./assets/logo.png")}
+          style={{width:350, height:350, marginLeft:0, alignItems:"CENTER"}}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            var word=this.state.text.toLowerCase().trim();
+            db[word]? (
+            this.setState({ chunks: db[word].chunks }),
+            this.setState({ phonicSounds: db[word].phones })):
+            Alert.alert("Word is unavailable");
+          }}>
+          <Text style={styles.buttonText}>🅶🅾</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item, index) => {
+            return (
+              <PhonicSoundButton
+                wordChunk={this.state.chunks[index]}
+                soundChunk={this.state.phonicSounds[index]}
+                buttonIndex={index}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFF66',
+  },
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
